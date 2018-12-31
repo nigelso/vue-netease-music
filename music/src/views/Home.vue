5b687373f1e41881b37e8bacfe8dee367d62e81c
nigelso <nigelso@163.com>
2018-12-31 20:24:25 +0800

    initial

---
+++ music/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang='stylus'>
+
+</style>
+
